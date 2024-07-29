aadfcc883f5e9688838ff790d723c1365bff02de
Sanya Moses Adeniyi <54499732+akpcode@users.noreply.github.com>
2024-07-29 22:00:59 +0100

    changes

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,11 @@ const App: React.FC = () => {
 
   const submitHandler = (event: React.FormEvent) => {
     event.preventDefault();
+    
+    //saving the inputs
+    localStorage.setItem('email', email);
+    localStorage.setItem('password', password);
+    
     // Authentication handler
     console.log('Email:', email);
     console.log('Password:', password);
@@ -14,14 +19,14 @@ const App: React.FC = () => {
   return (
     <div className="flex items-center justify-center min-h-screen ml-80 w-full bg-gray-600 bg-opacity-30">
       <div className="bg-white p-6 shadow-md w-full max-w-sm rounded-2xl">
-        <h2 className="text-2xl font-bold mb-6 text-center">Login</h2>
+        <h2 className="text-2xl font-bold mb-6 text-center text-red-400">Login</h2>
         <form onSubmit={submitHandler}>
           <div className="mb-4">
             <label htmlFor="email" className="block text-sm font-medium text-gray-700">Email</label>
             <input
               type="email"
               id="email"
-              className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+              className="mt-1 block w-full px-3 py-2 border-none bg-green-400 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
               value={email}
               onChange={(e) => setEmail(e.target.value)}
               required
@@ -32,7 +37,7 @@ const App: React.FC = () => {
             <input
               type="password"
               id="password"
-              className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+              className="mt-1 block w-full px-3 py-2 border-none bg-green-400 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
               value={password}
               onChange={(e) => setPassword(e.target.value)}
               required
